18b373f19efca12257b07e57951a8ed26f9bef28
RossHartigan <rosshartigan@nelsondevelopment.co.za>
2024-10-24 18:50:57 +0200

    changes

--- Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
+++ Shepherd-Parking-Website/obj/Debug/net8.0/Shepherd-Parking-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c49e9e1f1b84c9769b5833b7ae72fdf30078fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7796a3f294e0ebe3106b8de910bebaefd1466c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shepherd-Parking-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
